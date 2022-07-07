8ffd4131af1daa4720374b7080e953761e8bdf1f
powcoder代写 <powcoder@163.com>
2022-07-07 13:24:46 +0800

    程序代写 加微信 powcoder

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+mkdir build
+cd build
+cmake ..
+make clean
+make
+
+cd ../test/
+# One time event
+clang -emit-llvm -S -c -o ArrayDemo.bc ArrayDemo.c
+#clang -emit-llvm -S -c -o Greatest.bc Greatest.c
+
+#Full run
+../build/constraint -ReachDef ArrayDemo.bc
+#../build/constraint -Liveness ArrayDemo.bc
+
+#../build/constraint -ReachDef Greatest.bc
+#../build/constraint -Liveness Greatest.bc
+
+
+exit 
